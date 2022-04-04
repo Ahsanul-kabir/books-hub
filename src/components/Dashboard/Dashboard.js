f545ba34939510369be7c8ed51bf40204282a128
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-04-04 19:38:34 +0600

    graph reports added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import sellDataSet from '../../fakeData/sellData.json';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const [sellData, setSellData] = useState([])
+
+    useEffect(() => {
+        setSellData(sellDataSet)
+    }, [])
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="container">
+            <section className="row">
+                <div className="col-sm-8 col-md-12 col-lg-12">
+                    <h1 className="m-3 d-flex justify-content-center graph-heading">Monthly Sell Reports</h1>
+                    <ResponsiveContainer width="90%" height={300}>
+                        <LineChart data={sellData} >
+                            <Line type="monotone" dataKey="month" stroke="#8884d8" />
+                            <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" />
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <Tooltip />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </section>
+
+            <section className="row m-5">
+                <div className="col-sm-8 col-md-12 col-lg-12">
+                    <h1 className="m-3 d-flex justify-content-center graph-heading">Monthly Sell VS Reviews Reports</h1>
+                    <ResponsiveContainer width="95%" height={300}>
+                        <AreaChart width={730} height={250} data={sellData}
+                            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                            <defs>
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                </linearGradient>
+                                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                                </linearGradient>
+                            </defs>
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="sell" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                            <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+            </section>
+
+            <section className="row">
+                <div className="col-sm-8 col-md-12 col-lg-12">
+                    <h1 className="m-3 d-flex justify-content-center graph-heading">Monthly Investment vs Revenue Reports</h1>
+                    <ResponsiveContainer width="90%" height={300}>
+                        <BarChart width={730} height={250} data={sellData}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="investment" fill="#82ca9d" />
+                            <Bar dataKey="revenue" fill="#8884d8" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+            </section>
         </div>
     );
 };
